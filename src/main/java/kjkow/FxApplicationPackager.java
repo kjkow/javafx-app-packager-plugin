f710fb4c07a38ff73061803e8ba4ac1c74dab931
Kamil.Kowalczyk <karmel92@o2.pl>
2017-10-12 15:59:39 +0200

    initialize

---
+++ src/main/java/kjkow/FxApplicationPackager.java
@@ -0,0 +1,39 @@
+package kjkow;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+
+@Mojo(name = "fxPackager", defaultPhase = LifecyclePhase.INSTALL)
+public class FxApplicationPackager extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project.version}", readonly = true)
+    private String projectVerison;
+
+    @Parameter(defaultValue = "${project.build.directory}", readonly = true)
+    private File targetDirectory;
+
+    public void execute() throws MojoExecutionException {
+
+    }
+}
